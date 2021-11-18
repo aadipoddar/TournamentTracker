79c47ef8f7136595e7c93b32610d84ea3b1c3ec9
Aadi Poddar <aadipoddarmail@gmail.com>
2021-11-18 21:11:34 +0530

    Text Connection
    Create a Text Connection to store the data in ".csv" file

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -5,15 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModel.csv";
+
         // TODO - Wire up the CreatePrize for the text files.
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            // Load The Text File
+            // Convert the text to List<PrizeMode>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+
+            // Find the max ID
+            int currentId = 1;
+
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            model.Id = currentId;
+
+
+            // Add new record with the new ID (max + 1)
+            prizes.Add(model);
+
+
+            // Convert the prize to list<string>
+            // Save the list<string> to the test file
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
